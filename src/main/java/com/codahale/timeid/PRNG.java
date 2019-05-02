cfcc8f00a383d4a10fb686a3726842f21b95ce31
Coda Hale <coda.hale@gmail.com>
2019-05-01 17:25:07 -0700

    Erase PRNG state incrementally.

--- src/main/java/com/codahale/timeid/PRNG.java
+++ src/main/java/com/codahale/timeid/PRNG.java
@@ -18,6 +18,7 @@ package com.codahale.timeid;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.ShortBufferException;
@@ -26,12 +27,11 @@ import javax.crypto.spec.SecretKeySpec;
 
 /** PRNG generates pseudo-random data using AES-256-CTR in a fast-key-erasure construction. */
 class PRNG {
+  static final int KEY_LEN = 32;
   private static final int BLOCK_SIZE = 16;
   private static final IvParameterSpec IV = new IvParameterSpec(new byte[BLOCK_SIZE]);
   private static final int MAX_BLOCKS = 256;
   private static final int BUF_SIZE = BLOCK_SIZE * MAX_BLOCKS;
-  private static final byte[] ZEROS = new byte[BUF_SIZE];
-  static final int KEY_LEN = 32;
   private final Cipher aes;
   private final byte[] buffer;
   private int offset;
@@ -56,17 +56,20 @@ class PRNG {
       cycle();
     }
     System.arraycopy(buffer, offset, out, 4, BLOCK_SIZE);
+    Arrays.fill(buffer, offset, offset + BLOCK_SIZE, (byte) 0);
     offset += BLOCK_SIZE;
   }
 
   private void cycle() {
     try {
       // Use the current key to encrypt a number of zero blocks.
-      aes.update(ZEROS, 0, BUF_SIZE, buffer, 0);
+      aes.update(buffer, 0, BUF_SIZE, buffer, 0);
       // Use the first two encrypted blocks as the new key.
       aes.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(buffer, 0, KEY_LEN, "AES"), IV);
       // Don't use the key as output.
       offset = KEY_LEN;
+      // Zero out the key.
+      Arrays.fill(buffer, 0, KEY_LEN, (byte) 0);
     } catch (ShortBufferException | InvalidKeyException | InvalidAlgorithmParameterException e) {
       throw new IllegalStateException(e);
     }
