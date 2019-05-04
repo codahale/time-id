1bec2ea00bd26be1116eeef14c6e7e6ff6d4d188
Coda Hale <coda.hale@gmail.com>
2019-05-04 11:52:38 -0700

    Provide more detailed benchmark results.

--- src/test/java/com/codahale/timeid/benchmarks/Benchmarks.java
+++ src/test/java/com/codahale/timeid/benchmarks/Benchmarks.java
@@ -16,16 +16,10 @@
 package com.codahale.timeid.benchmarks;
 
 import com.codahale.timeid.IdGenerator;
-import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class Benchmarks {
   private final IdGenerator generator = new IdGenerator();
